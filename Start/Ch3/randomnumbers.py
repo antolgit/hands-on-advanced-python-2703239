bb28bd73bea0601acf54a0f241e501c540314532
antolgit <158453161+antolgit@users.noreply.github.com>
2024-03-15 09:48:59 +0000

    Pending changes exported from your codespace

--- Start/Ch3/randomnumbers.py
+++ Start/Ch3/randomnumbers.py
@@ -9,12 +9,12 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: the random module can be used to generate random values
-
+# print(random.random())
 
 # TODO: choose a random number in a range including both points
-
+# print(random.randint(10,20))
 # TODO: choose a random number in a range excluding end point
-
+# print(random.randrange(10,20))
 
 # build a list of the summer days in 2019
 def is_summer_day(d):
@@ -25,6 +25,11 @@ def is_summer_day(d):
 summer_2019 = list(filter(is_summer_day, weatherdata))
 
 # TODO: choose 5 random days from that summer
-
+# use seed to force which random numers are used (reproduceable)
+random.seed(10)
+random_days = []
+for _ in range(5):
+    random_days.append(summer_2019[random.randrange(len(summer_2019))])
 
 # TODO: what was the windiest of those 5 days?
+print(max(random_days, key=lambda d:d['awnd']))
